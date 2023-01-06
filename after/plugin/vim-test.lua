9578702702332b6f3de4487d5afc2bac0f68cc07
Jeremy W. Rowe <jeremy.rowe@shopify.com>
2023-01-06 14:58:16 -0500

    add vtr config

--- after/plugin/vim-test.lua
+++ after/plugin/vim-test.lua
@@ -1,8 +1,14 @@
 vim.keymap.set('n', '<leader>tf', function () vim.cmd('TestFile') end)
 vim.keymap.set('n', '<leader>tl', function () vim.cmd('TestLast') end)
+vim.keymap.set('n', '<leader>ra', function () vim.cmd('VtrAttachToPane') end)
+vim.keymap.set('n', '<leader>rf', function () vim.cmd('VtrFocusRunner') end)
 
 if os.getenv('TMUX') then
     vim.g['test#strategy'] = 'vtr'
 else
     vim.g['test#strategy'] = 'neovim'
 end
+
+vim.g['VtrPercentage'] = 35
+vim.g['VtrOrientation'] = "h"
+vim.g['VtrClearBeforeSend'] = 0
